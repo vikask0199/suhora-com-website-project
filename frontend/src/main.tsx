c5e9aa3e1cb4f46c2cfa1bdf6a355c9a36c54595
Vikas Kumar <vikasku0199@gmail.com>
2024-02-07 21:30:03 +0530

    first code vikas

---
+++ frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/*' element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+)
