f86fd5f097f23cf5962902c81ec50ace101f85f2
Hamna34 <khanhamna399@gmail.com>
2024-02-09 13:41:51 +0530

    adding images

--- frontend/src/components/principles/Mision.tsx
+++ frontend/src/components/principles/Mision.tsx
@@ -4,17 +4,17 @@ import cc from '../../assets/img/mission.jpg'
 const Mision = () => {
   return (
    <>
-   <div className='bg-gray-200 p-4'>
+   <div className='principle p-4'>
    <div className=' ml-[10vh] mr-[10-vh] flex gap-4 ' >
-   <div className="max-w-sm rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-[10vh]">
+   <div className="max-w-sm rounded-lg shadow d  p-[10vh]">
     <a href="#">
         
     </a>
     <div className="p-5">
         <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Who Are we?</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight ">Who Are we?</h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <p className="mb-3 font-normal ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
         <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
              
